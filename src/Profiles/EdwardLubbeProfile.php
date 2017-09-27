7f79c01d5b3dea499771887aa7db99494df2935d
Edward Lubbe <edwardlubbe@gmail.com>
2017-09-27 14:55:30 +0200

    My profile

---
+++ src/Profiles/EdwardLubbeProfile.php
@@ -0,0 +1,21 @@
+<?php
+
+    namespace mostertb\PHPSA2017Profiles\Profiles;
+
+    class DuwayneBrownProfile extends AbstractProfile
+    {
+        public function getName()
+        {
+            return 'Edward Lubbe';
+        }
+
+        public function getBiography()
+        {
+            return 'http://www.edwardlubbe.co.za';
+        }
+
+        public function getGitHubUsername()
+        {
+            return 'http://www.github.com/edwardlubbe';
+        }
+    }
