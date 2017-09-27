87eb38fc0985a1d409f3057d039688a23d9b09a3
vauneen <vauneen@gmail.com>
2017-09-27 16:47:46 +0200

    Add files via upload

---
+++ src/Profiles/VauneenPietersenProfile.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace mostertb\PHPSA2017Profiles\Profiles;
+
+
+class VauneenPietersenBio extends AbstractProfile
+{
+
+    
+
+// Implement the following methods:
+
+// getName()
+// getDescription()
+// getGitHubUsername()
+// getProfileImageURL() (optional)
+// getMaintainedProjects() (optional)
+
+
+    public function getName()
+    {
+        return 'Vauneen Pietersen';
+    }
+
+
+    public function getDescription()
+    {
+        return 'description: human';
+    }
+
+
+    public function getGitHubUsername()
+    {
+        return 'vauneen';
+    }
+
+
+    public function getProfileImageURL()
+    {
+        return 'http://graph.facebook.com/530448473/picture/';
+    }
+
+
+}
