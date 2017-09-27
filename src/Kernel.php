6e1efbc2851974275701810a2d64cb33c7ef10e8
Danie-ZA <daniel-cilliers@live.com>
2017-09-27 14:17:14 +0200

    DanielCilliersProfile

--- src/Kernel.php
+++ src/Kernel.php
@@ -3,6 +3,7 @@ namespace mostertb\PHPSA2017Profiles;
 
 use mostertb\PHPSA2017Profiles\Profiles\AbstractProfile;
 use mostertb\PHPSA2017Profiles\Profiles\BradMostertProfile;
+use mostertb\PHPSA2017Profiles\Profiles\DanielCilliersProfile;
 
 class Kernel
 {
@@ -17,7 +18,7 @@ class Kernel
     public function __construct()
     {
         $this->profiles = array(
-            new BradMostertProfile()
+            new DanielCilliersProfile()
         );
     }
 
