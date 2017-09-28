635f068140bd2e4d30574cb331858cfff65ca1dd
Brad Mostert <mostertb@users.noreply.github.com>
2017-09-28 14:04:19 +0200

    Fix fatal error from duplicate use statement

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,21 +1,20 @@
 <?php
 namespace mostertb\PHPSA2017Profiles;
 
-use mostertb\PHPSA2017Profiles\Profiles\AbstractProfile;
-use mostertb\PHPSA2017Profiles\Profiles\BradMostertProfile;
-use mostertb\PHPSA2017Profiles\Profiles\DuwayneBrownProfile;
-use mostertb\PHPSA2017Profiles\Profiles\RonDarbyProfile;
 use mostertb\PHPSA2017Profiles\Profiles\AbdurahimShariffProfile;
+use mostertb\PHPSA2017Profiles\Profiles\AbstractProfile;
+use mostertb\PHPSA2017Profiles\Profiles\AkinyeleOlubodunProfile;
+use mostertb\PHPSA2017Profiles\Profiles\AlexBlakeProfile;
+use mostertb\PHPSA2017Profiles\Profiles\BradMostertProfile;
 use mostertb\PHPSA2017Profiles\Profiles\DanielCilliersProfile;
-use mostertb\PHPSA2017Profiles\Profiles\RoyFoubisterProfile;
+use mostertb\PHPSA2017Profiles\Profiles\DuwayneBrownProfile;
+use mostertb\PHPSA2017Profiles\Profiles\EdwardLubbeProfile;
+use mostertb\PHPSA2017Profiles\Profiles\EtienneMaraisProfile;
 use mostertb\PHPSA2017Profiles\Profiles\JohnMcMurrayProfile;
 use mostertb\PHPSA2017Profiles\Profiles\JohnRouxProfile;
 use mostertb\PHPSA2017Profiles\Profiles\KittyProfile;
-use mostertb\PHPSA2017Profiles\Profiles\AkinyeleOlubodunProfile;
-use mostertb\PHPSA2017Profiles\Profiles\AbdurahimShariffProfile;
-use mostertb\PHPSA2017Profiles\Profiles\EdwardLubbeProfile;
-use mostertb\PHPSA2017Profiles\Profiles\EtienneMaraisProfile;
-use mostertb\PHPSA2017Profiles\Profiles\AlexBlakeProfile;
+use mostertb\PHPSA2017Profiles\Profiles\RonDarbyProfile;
+use mostertb\PHPSA2017Profiles\Profiles\RoyFoubisterProfile;
 
 class Kernel
 {
