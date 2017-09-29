615b38d7605e6ad1e1ce78dc84b86370ec9dc7fa
Kyle Hickson <hickson.kyle@gmail.com>
2017-09-29 07:49:52 +0200

    Add Kyle Hickson profile

---
+++ src/Profiles/KyleHicksonProfile.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace mostertb\PHPSA2017Profiles\Profiles;
+
+class KyleHicksonProfile extends AbstractProfile
+{
+    public function getName()
+    {
+        return 'Kyle Hickson';
+    }
+
+    public function getBiography()
+    {
+        return 'Senior developer at <a href="https://za.linkedin.com/company/tencent-africa" target="_blank">Tencent 
+                    Africa Services</a> and passionate about machine learning ';
+    }
+
+    public function getGitHubUsername()
+    {
+        return 'KillaZA';
+    }
+
+    public function getProfileImageURL()
+    {
+        return 'https://s.gravatar.com/avatar/57657257c94ea11ff998cd89a90898c6?s=80';
+    }
+}
