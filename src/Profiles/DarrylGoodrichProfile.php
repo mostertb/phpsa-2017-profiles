2b6bc128daeb72b2a5b1b0032f74188bb48bd371
Darryl Goodrich <darryl_goodrich@yahoo.com>
2017-09-29 15:38:50 +0200

    DarrylGoodrichProfile.php

---
+++ src/Profiles/DarrylGoodrichProfile.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace mostertb\PHPSA2017Profiles\Profiles;
+
+use mostertb\PHPSA2017Profiles\Abstracts\AbstractProfile;
+
+class DarrylGoodrichProfile extends AbstractProfile
+{
+
+    /**
+     * Provides the full name of the person that the bio is about
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'Darryl Goodrich';
+    }
+
+    /**
+     * Provides a paragraph of text about the person that the profile is about
+     *
+     * @return string
+     */
+    public function getBiography()
+    {
+        return 'Recently joined Zando';
+    }
+
+    /**
+     * Should return a publicly accessible URL to an image of the person that the bio is about.
+     *
+     * @return null|string
+     */
+    public function getProfileImageURL()
+    {
+        return 'https://avatars1.githubusercontent.com/u/17751794?v=4&s=460';
+    }
+
+    /**
+     * Should return the GitHub username of the person the bio is about. This is used to generate links to the person's
+     * OpenSource projects
+     *
+     * @return string
+     */
+    public function getGitHubUsername()
+    {
+        return 'DGoodrich';
+    }
+
+}
