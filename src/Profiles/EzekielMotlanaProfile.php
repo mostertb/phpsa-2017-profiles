31b36326c65460811e95babdd7fe4408b656aa3c
Ezekiel Motlana <ezekielm@miway.co.za>
2017-10-04 15:46:11 +0200

    Add Ezekiel Motlana's Profile

---
+++ src/Profiles/EzekielMotlanaProfile.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace mostertb\PHPSA2017Profiles\Profiles;
+
+
+use mostertb\PHPSA2017Profiles\Abstracts\AbstractProfile;
+
+class EzekielMotlanaProfile extends AbstractProfile
+{
+
+    /**
+     * Provides the full name of the person that the bio is about
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'Ezekiel Motlana';
+    }
+
+    /**
+     * Provides a paragraph of text about the person that the profile is about
+     *
+     * @return string
+     */
+    public function getBiography()
+    {
+        return 'Integration Developer at Miway Insurance Limited. API Fanatic';
+    }
+
+    /**
+     * Should return a publicly accessible URL to an image of the person that the bio is about.
+     *
+     * @return null|string
+     */
+    public function getProfileImageURL()
+    {
+        return 'https://en.gravatar.com/userimage/7370015/005a6596f5ff689fdaae5026b51524d2?size=200';
+    }
+
+    /**
+     * Should return the GitHub username of the person the bio is about. This is used to generate links to the person's
+     * OpenSource projects
+     *
+     * @return string
+     */
+    public function getGitHubUsername()
+    {
+        return 'mosekwa';
+    }
+
+    /**
+     * Optional function that should return an array of OpenSource projects maintained by this person.
+     * The index should contain the name of the project and the value should be a url to the project's homepage (typically
+     * a GitHub page)
+     *
+     * @return array
+     */
+    public function getMaintainedProjects()
+    {
+        return array(
+            'mostertb/phpsa-2017-profiles' => 'https://github.com/mostertb/phpsa-2017-profiles',
+        );
+    }
+
+    /**
+     * Optional function that should return an array of OpenSource projects to which this person has contributed (or at least
+     * recommends). The index should contain the name of the project and the value should be a url to the project's
+     * homepage (typically a GitHub page)
+     *
+     * @return array
+     */
+    public function getInvolvedProjects()
+    {
+        return array();
+    }
+}
